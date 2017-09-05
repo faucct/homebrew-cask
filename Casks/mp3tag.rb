c8f6fc475441ae1d05be88551984a915d9fa2e70
faucct <faucct@gmail.com>
2017-09-05 14:42:40 +0300

    Update mp3tag.rb

--- Casks/mp3tag.rb
+++ Casks/mp3tag.rb
@@ -1,5 +1,5 @@
 cask 'mp3tag' do
-  version '2.82'
+  version '2.84'
   sha256 'a74dfc61889240468b2d4c05c86a712a2c148162e777696782007f09c3c9f87b'
 
   url "http://download.mp3tag.de/mp3tagv#{version.no_dots}-MacOSX-Wine.zip"
